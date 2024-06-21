47e50c04928c454c7ef0421feeaf3c33fdef4716
André <andre.m.varandas@gmail.com>
2024-06-21 18:14:14 +0100

    fix: add release config

---
+++ .releaserc.mjs
@@ -0,0 +1,12 @@
+import commitAnalyzer from '@semantic-release/commit-analyzer'
+import releaseNotesGenerator from '@semantic-release/release-notes-generator'
+import changelog from '@semantic-release/changelog'
+import npm from '@semantic-release/npm'
+import github from '@semantic-release/github'
+import git from '@semantic-release/git'
+
+export default {
+  branches: ['main'],
+  repositoryUrl: 'https://github.com/AndreVarandas/piston.git',
+  plugins: [commitAnalyzer, releaseNotesGenerator, changelog, npm, github, git],
+}
